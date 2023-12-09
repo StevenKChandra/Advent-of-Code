b28bf784b626c2c560264ffc3679c0c4a2ac0b20
Steven Kurniawan Chandra <steven.k.chandra@gmail.com>
2023-12-09 15:07:40 +0000

    finished day 09

---
+++ 2023/day 09/part 2.py
@@ -0,0 +1,33 @@
+import re
+
+def sum_extrapolation(filepath):
+    """ """
+
+    with open(filepath, "r") as f: # open the file
+        
+        sum = 0
+        for line in f: # iterate through the text
+            
+            # strip the endlines
+            line = line.strip()
+
+            history = re.findall("-?[\d]+", line)
+            history = [int(number) for number in history]
+            sum += extrapolate_prev(history)
+
+    return sum
+
+def extrapolate_prev(datas):
+    data_length = len(datas)
+
+    if datas == [0]*(data_length-1):
+        return 0
+    
+    difference = []
+    for i in range(data_length-1):
+        difference.append(datas[i+1]-datas[i])
+
+    return datas[0] - extrapolate_prev(difference)
+
+if __name__ == "__main__":
+    print(sum_extrapolation("input.txt"))
