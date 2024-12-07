ab3220b3d295bdacb07989a12b9443a360eb1cee
Steven Kurniawan Chandra <94112051+StevenKChandra@users.noreply.github.com>
2024-12-07 12:02:25 +0000

    Added solution for day 07

---
+++ 2024/day 07/part 2.py
@@ -0,0 +1,28 @@
+with open("2024/day 07/input.txt") as f:
+    input = f.read().strip().split("\n")
+
+total = 0
+
+def operate(numbers):
+    output = [numbers[0]]
+
+    for number in numbers[1:]:
+        placeholder = []
+        for item in output:
+            placeholder.append(item + number)
+            placeholder.append(item * number)
+            placeholder.append(int(str(item)+str(number)))
+        output = placeholder
+
+    return output
+
+for row in input:
+    target, numbers = row.split(": ")
+
+    target = int(target)
+    numbers = list(map(int, numbers.split()))
+
+    if target in operate(numbers):
+        total += target
+
+print(total)
