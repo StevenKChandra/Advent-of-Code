2c441dc21d36981b5d91d1df230126f202d387a7
Steven Kurniawan Chandra <94112051+StevenKChandra@users.noreply.github.com>
2024-12-09 14:30:15 +0000

    Added solution for day 09

---
+++ 2024/day 09/part 1.py
@@ -0,0 +1,50 @@
+with open("2024/day 09/input.txt", "r") as f:
+# with open("2024/day 09/example.txt", "r") as f:
+    disk_map = f.read().strip()
+
+spaces = disk_map[1::2]
+blocks = disk_map[::2]
+
+m = len(spaces)
+n = len(blocks)
+
+file_ID_front = 0
+file_ID_back = n - 1
+
+check_sum = 0
+sequence_counter = 0
+remainder = int(blocks[file_ID_back])
+
+while file_ID_front <= file_ID_back:
+
+    block_to_fill = int(blocks[file_ID_front])
+    check_sum += block_to_fill * (2 * sequence_counter + block_to_fill - 1) * file_ID_front / 2
+    sequence_counter += block_to_fill
+    file_ID_front += 1
+
+    if file_ID_back == 5350:
+        pass
+
+    if file_ID_front >= file_ID_back:
+        break
+    
+    space_to_fill = int(spaces[file_ID_front - 1])
+
+    while space_to_fill >= remainder:
+        check_sum += remainder * (2 * sequence_counter + remainder - 1) * file_ID_back / 2
+        sequence_counter += remainder
+        space_to_fill -= remainder
+        file_ID_back -= 1
+        remainder = int(blocks[file_ID_back])
+    
+    if file_ID_front >= file_ID_back:
+        remainder = 0
+        break
+
+    check_sum += space_to_fill * (2 * sequence_counter + space_to_fill - 1) * file_ID_back / 2
+    sequence_counter += space_to_fill
+    remainder -= space_to_fill
+
+check_sum += remainder * (2 * sequence_counter + remainder - 1) * file_ID_back / 2
+
+print(check_sum)
---
+++ 2024/day 09/part 2.py
@@ -0,0 +1,38 @@
+with open("2024/day 09/input.txt", "r") as f:
+# with open("2024/day 09/example.txt", "r") as f:
+    disk_map = f.read().strip()
+
+spaces = list(map(int,disk_map[1::2]))
+spaces.append(0)
+blocks = list(map(int,disk_map[::2]))
+
+blocks_copy = blocks.copy()
+spaces_copy = spaces.copy()
+
+m = len(spaces)
+n = len(blocks)
+file_ID_front = 0
+file_ID_back = n - 1
+
+
+check_sum = 0
+
+for i in reversed(range(n)):
+    sequence_counter = 0
+    for j in range(i):
+        sequence_counter += int(blocks[j])
+        if blocks_copy[i] <= spaces[j]:
+            check_sum += blocks_copy[i] * (2 * sequence_counter + blocks_copy[i] - 1) * i / 2
+            spaces[j] -= blocks_copy[i]
+            blocks[j] += blocks_copy[i]
+            blocks[i] = 0
+            break
+        sequence_counter += spaces[j]
+
+sequence_counter = 0
+for i in range(n):
+    if blocks[i] != 0:
+        check_sum += blocks_copy[i] * (2 * sequence_counter + blocks_copy[i] - 1) * i / 2
+    sequence_counter += blocks_copy[i] + spaces_copy[i]
+
+print(check_sum)
